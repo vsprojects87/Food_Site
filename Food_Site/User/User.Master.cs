fa1e09771eeee7158b10deb053b0a15fd112bdf3
vsprojects87 <vs.scala.619@gmail.com>
2024-01-16 15:07:47 +0530

    #2.0

---
+++ Food_Site/User/User.Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Food_Site.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx")) {
+                form1.Attributes.Add("class","sub_page");
+            }
+            else
+            {
+                form1.Attributes.Remove("class");
+                // load the control
+                Control SliderUserControl = (Control)Page.LoadControl("SliderUSerControl1.ascx");
+                pnlSliderUC.Controls.Add(SliderUserControl);
+            }
+        }
+    }
+}
