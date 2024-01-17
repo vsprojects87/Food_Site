90c5b3af7e7414421d03176ca9a6c1a5d0a1aa42
vsprojects87 <vs.scala.619@gmail.com>
2024-01-17 18:13:42 +0530

    #3.0

---
+++ Food_Site/Admin/Category.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Food_Site.Admin
+{
+    public partial class Category : System.Web.UI.Page
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        DataTable dt;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAddOrUpdate_Click(object sender, EventArgs e)
+        {
+
+            string actioName= string.Empty, imagePath= string.Empty, fileExtention= string.Empty;
+            bool isValidToExecute = false;
+            int CategoryId = Convert.ToInt32(hdnId.Value);
+            con = new SqlConnection(Connection.GetConnectionString());
+            cmd= new SqlCommand("Category_Crud", con);
+            cmd.Parameters.AddWithValue("@Action", CategoryId = 0 ? "INSERT" : "UPDATE");
+            cmd.Parameters.AddWithValue("@CategoryId", CategoryId);
+            cmd.Parameters.AddWithValue("@Name",txtName.Text.Trim());
+            cmd.Parameters.AddWithValue("@IsActive", cbIsActive.Checked);
+            if(fuCategoryImage.HasFile)
+            {
+                if(Utils.IsValidExtention(fuCategoryImage.FileName))
+                {
+                    Guid obj = Guid.NewGuid();
+                    fileExtention = Path.GetExtension(fuCategoryImage.FileName);
+                    imagePath= "Images/Category/" + obj.ToString() + fileExtention;
+                    fuCategoryImage.PostedFile.SaveAs(Server.MapPath("~/Images/Category") + obj.ToString() + fileExtention);
+                    cmd.Parameters.AddWithValue("@ImageUrl", imagePath);
+                    isValidToExecute = true;
+                }
+                else
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Please select .jpg, .jpeg or .png images";
+                    lblMsg.CssClass = "alert alert-danger";
+                    isValidToExecute= false;
+                }
+            }
+            else
+            {
+                isValidToExecute= true;
+            }
+
+            if (isValidToExecute)
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    actioName = CategoryId == 0 ? "inserted" : "updated";
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Category" + actioName + "successfully";
+                    lblMsg.CssClass = "alert alert-danger";
+                    getCategories();
+                    clear();
+                }
+                catch(Exception ex)
+                {
+                    lblMsg.Visible = true;
+                    lblMsg.Text = "Error-" + ex.Message;
+                    lblMsg.CssClass= "alert alert-danger";
+                }
+                finally
+                {
+                    con.Close();
+                } 
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Food_Site/Connection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Food_Site
+{
+    public class Connection
+    {
+       public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString; 
+        }
+    }
+
+
+    public class Utils
+    {
+        public static bool IsValidExtention(string filenName)
+        {
+            bool isValid = false;
+            string[] fileExtension = {".jpg",".png",".jpeg" };
+            for(int i = 0; i <= fileExtension.Length - 1; i++) {
+                if (filenName.Contains(fileExtension[i]))
+                {
+                    isValid = true;
+                    break;
+                }
+            }
+            return isValid;
+         }
+    }
+}
